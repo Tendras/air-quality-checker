0f1b48cf4bd716691da4144ac188f2646d6c29f8
Andrei Bardac <bardacandrei2@gmail.com>
2024-11-17 09:34:45 +0100

    add dynamic map in frontend

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,1 +1,29 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron');
+
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
+  });
+
+  win.loadFile('main.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
